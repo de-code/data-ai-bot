1330e40093c49a28bc82cac43b14ae8dd2321ad3
Daniel Ecer <de-code@users.noreply.github.com>
2025-03-05 16:50:15 +0000

    Added joke tool (#12)

---
+++ data_ai_bot/tools/example/joke.py
@@ -0,0 +1,40 @@
+# Mostly copied from smolagents examples
+
+import logging
+
+import requests
+
+from smolagents import tool  # type: ignore
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+@tool
+def get_joke() -> str:
+    '''
+    Fetches a random joke from the JokeAPI.
+    This function sends a GET request to the JokeAPI to retrieve a random joke.
+    It handles both single jokes and two-part jokes (setup and delivery).
+    If the request fails or the response does not contain a joke, an error message is returned.
+    Returns:
+        str: The joke as a string, or an error message if the joke could not be fetched.
+    '''
+    url = 'https://v2.jokeapi.dev/joke/Programming?type=single&safe-mode&blacklistFlags=nsfw'
+    LOGGER.info('url: %r', url)
+
+    try:
+        response = requests.get(url, timeout=30)
+        response.raise_for_status()
+
+        data = response.json()
+        LOGGER.info('data: %r', data)
+
+        if 'joke' in data:
+            return data['joke']
+        if 'setup' in data and 'delivery' in data:
+            return f'{data['setup']} - {data['delivery']}'
+        return 'Error: Unable to fetch joke.'
+
+    except requests.exceptions.RequestException as e:
+        return f'Error fetching joke: {str(e)}'
